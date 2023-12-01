a5ebcf3a3790c69b64e3d07036b22ffdf21933d4
Eric de Souza Porto Carvalho <ericsporto@gmail.com>
2023-12-01 09:35:28 -0300

    fix: change photos

--- src/components/MusiciansTabs/PaoTab.tsx
+++ src/components/MusiciansTabs/PaoTab.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function PaoTab() {
   return (
     <div className="p-1 xl:p-4 w-full h-full flex flex-col xl:flex-row items-center justify-center gap-2 xl:gap-8">
-      <div className='w-[216px] h-[315px] xl:h-96 xl:-mt-20'
+      <div className='w-64 h-96 xl:h-96 xl:-mt-20'
         style={{
           backgroundImage: `url(/assets/pao-1.jpg)`,
           backgroundRepeat: 'no-repeat',
