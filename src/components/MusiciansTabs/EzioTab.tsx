1a60b155de733b1d9684ef06d5ec530dc02cf1e5
Eric de Souza Porto Carvalho <ericsporto@gmail.com>
2023-12-01 09:22:11 -0300

    fix: change photos

--- src/components/MusiciansTabs/EzioTab.tsx
+++ src/components/MusiciansTabs/EzioTab.tsx
@@ -3,12 +3,12 @@ import React from 'react';
 function EzioTab() {
   return (
     <div className="p-1 xl:p-4 w-full h-full flex flex-col xl:flex-row items-center justify-center gap-2 xl:gap-8">
-      <div className='w-[216px] h-96 xl:-mt-20'
+      <div className='w-64 h-96 xl:-mt-20'
         style={{
           backgroundImage: `url(/assets/ezio-1.jpg)`,
           backgroundRepeat: 'no-repeat',
           backgroundPosition: 'center',
-          backgroundSize: 'contain',
+          backgroundSize: 'cover',
         }}
       />
       <div className='w-64 h-96 xl:mt-20'
